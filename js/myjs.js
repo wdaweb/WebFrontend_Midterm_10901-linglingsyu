b55807cddb432f07a846fd422067e99c67eadac3
linglingsyu <zzxcv741@hotmail.com>
2020-07-14 16:32:51 +0800

    123

--- js/myjs.js
+++ js/myjs.js
@@ -1,12 +1,11 @@
 
-// let str = ["你好！我叫許瑞玲","歡迎您來參訪我的履歷^_^","了解我更多..."];
-// let count = 0;
-// let ani = $("#ani");
-// let text = $("#text");
 
-// if(ani.hasClass("typewriter")){
-//   ani.removeClass("typewriter");
-// }else{
-//   ani.addClass("typewriter");
-// }
-
+function todosort(sort){
+  let name = $(".card");
+  if(sort == "all"){
+    sort = "";
+  }
+  for ( let i = 0; i < name.length; i++){
+    name.eq(i).addClass("show");
+  }
+}
