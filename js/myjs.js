3710abc690161014de0ecd88606a99fa0e3f9e64
linglingsyu <zzxcv741@hotmail.com>
2020-07-22 00:25:25 +0800

    排版

--- js/myjs.js
+++ js/myjs.js
@@ -40,6 +40,19 @@ $("a[href*='#']").click(function() {
 }); 
 
 
+$(".totop").click(function(){
+  jQuery("html,body").animate({
+      scrollTop:0
+  },1000);
+});
+$(window).scroll(function() {
+  if ( $(this).scrollTop() > 300){
+      $('.totop').fadeIn("fast");
+  } else {
+      $('.totop').stop().fadeOut("fast");
+  }
+});
+
 
 
 
